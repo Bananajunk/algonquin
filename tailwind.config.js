8b06b04f5a00c49e12ff49d7c7cddd29f3f37cb9
Liam Reid <cl2reid@gmail.com>
2023-05-22 20:43:57 -0400

    setup splash header and image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        source: ["var(--font-source)"],
+        script: ["var(--font-script)"],
+      },
+      colors: {
+        base: "#060404",
+        background: "#f3eded",
+        primary: "#ffec38",
+        secondary: "#e6e6db",
+        accent: "#5e5e40",
       },
     },
   },
   plugins: [],
-}
+};
