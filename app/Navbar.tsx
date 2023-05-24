98f9ccd2a1e7f019c6489b30d6921bdfa2019799
Liam Reid <cl2reid@gmail.com>
2023-05-24 14:56:58 -0400

    updated navbar alignment

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import { HomeModernIcon } from "@heroicons/react/24/solid";
 
+import styles from "./Navbar.module.css";
+
 const Navbar = () => {
   return (
-    <nav className="flex justify-center absolute top-0 w-screen p-5 z-10 shadow-2xl font-heading">
-      <ul className="flex gap-8 items-center text-2xl">
-        <li>
-          <Link href="/">
-            <HomeModernIcon className="h-8 w-8" />
-          </Link>
-        </li>
+    <nav
+      className={`${styles.Navbar} absolute top-0 w-screen p-5 z-10 shadow-2xl font-heading`}
+    >
+      <Link className={styles.HomeIcon} href="/">
+        <HomeModernIcon className="h-8 w-8" />
+      </Link>
+      <ul className={`${styles.NavMenu} flex gap-8 items-center text-2xl`}>
         <li>
           <Link href="/#memoir">Memoir</Link>
         </li>
