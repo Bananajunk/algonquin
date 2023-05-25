fbc6935983b0bca9a9caec8506f18a517ebaa259
Liam Reid <cl2reid@gmail.com>
2023-05-24 20:42:41 -0400

    add aria label for nav home icon

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <nav
       className={`${styles.Navbar} absolute top-0 w-screen p-5 z-10 shadow-2xl font-heading`}
     >
-      <Link className={styles.HomeIcon} href="/">
+      <Link className={styles.HomeIcon} href="/" aria-label="Home">
         <HomeModernIcon className="h-8 w-8" />
       </Link>
       <ul className={`${styles.NavMenu} flex gap-8 items-center text-2xl`}>
